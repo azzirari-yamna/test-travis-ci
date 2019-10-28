1beaac8329a934c92432f8585129868f5b63de39
yamna-azzirari <azzirari.y@gmail.com>
2019-10-28 16:26:59 +0100

    firt commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016, 2019 Openworx - Mario Gielissen
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
 {
     "name": "Openworx Material Backend Theme V13",
     "summary": "Openworx Material Backend Theme V13",
@@ -29,6 +25,5 @@
         #'views/sidebar.xml',
     ],
     #'live_test_url': 'https://youtu.be/JX-ntw2ORl8'
-
 }
 
--- models/res_users.py
+++ models/res_users.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016, 2019 Openworx
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
 from odoo import models, fields
 
 class ResUsers(models.Model):
-
     _inherit = 'res.users'
-
     sidebar_visible = fields.Boolean("Show App Sidebar", default=True)
-
     def __init__(self, pool, cr):
         """ Override of __init__ to add access rights on notification_email_send
             and alias fields. Access rights are disabled by default, but allowed
